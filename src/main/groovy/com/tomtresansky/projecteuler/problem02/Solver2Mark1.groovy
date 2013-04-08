2b8efad176812d5f78fea1938c84a7563b19855c
tomwork <ttresans@courts.state.ny.us>
2013-04-08 13:12:17 -0400

    Untested (though looks ok) solution to problem 2.

--- src/main/groovy/com/tomtresansky/projecteuler/problem02/Solver2Mark1.groovy
+++ src/main/groovy/com/tomtresansky/projecteuler/problem02/Solver2Mark1.groovy
@@ -1,10 +1,40 @@
 package com.tomtresansky.projecteuler.problem02
 
 class Solver2Mark1 {
-  def sum = 0;
+  private ArrayList<Long> knownFibs = [0G, 1G]
 
-  def nextFib = {
-    i0, i1 -> i0 + i1
+  def fib(i) {
+      //println "Calcing ${i}"
+
+      if (knownFibs.size > i) {
+        knownFibs[i]
+
+        //println "Remembered fib(${i}) = ${knownFibs[i]}"
+      } else {
+        knownFibs.ensureCapacity(i)
+        knownFibs[i] = fib(i - 1) + fib(i - 2)
+
+        //println "Found fib(${i}) = ${knownFibs[i]}"
+      }
+
+      return knownFibs[i]
+  }
+
+  def solve() {
+    def sum = 0, lastFib = 0, i = 0
+
+    while (true) {
+      lastFib = fib(i++)
+
+      if (lastFib > 4000000) break;
+      if (lastFib % 2 == 0) sum += lastFib
+    }
+
+    sum
+  }
+
+  static void main(String... args) {
+    println ("\nProblem 2: " + new Solver2Mark1().solve())
   }
 }
 
