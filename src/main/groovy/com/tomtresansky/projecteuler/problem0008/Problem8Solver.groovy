9580bb13b45ff34d090ccfd57a71cab76df0664a
tomwork <ttresans@courts.state.ny.us>
2013-04-16 12:50:25 -0400

    Added problem 8 solution.

---
+++ src/main/groovy/com/tomtresansky/projecteuler/problem0008/Problem8Solver.groovy
@@ -0,0 +1,7 @@
+package com.tomtresansky.projecteuler.problem0008
+
+import com.tomtresansky.projecteuler.ProblemSolver;
+
+interface Problem8Solver extends ProblemSolver {
+
+}
---
+++ src/test/groovy/com/tomtresansky/projecteuler/problem0008/Solver8Mark1Test.groovy
@@ -0,0 +1,10 @@
+package com.tomtresansky.projecteuler.problem0008
+
+import org.junit.Test
+
+class Solver8Mark1Test extends GroovyTestCase {
+  @Test
+  void testSolution() {
+    assert 40824 == new Solver8Mark1().solve()
+  }
+}
