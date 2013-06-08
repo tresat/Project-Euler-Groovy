5511f337ef5a0a90009201393966794f73f738f2
Tom Tresansky <tom.tresansky@gmail.com>
2013-06-08 14:43:01 -0400

    This attempt at speeding up the collatz generator isn't working: slows way down and runs out of memory during GC.
    -But why?

--- src/main/groovy/com/tomtresansky/projecteuler/util/CollatzGenerator.groovy
+++ src/main/groovy/com/tomtresansky/projecteuler/util/CollatzGenerator.groovy
@@ -1,39 +1,86 @@
 package com.tomtresansky.projecteuler.util
 
 class CollatzGenerator {
-  private collatz = [:]
+  Map collatz = [:].withDefault { key -> generateCollatz(key) }
 
   private List generateCollatz(n) {
     assert n > 0
 
     BigInteger remaining = n
-    def result = [remaining]
+    List result = [remaining]
     while (remaining != 1) {
       //println "testing ${remaining}"
       if (collatz.containsKey(remaining.toString())) {
         //println "found ${remaining}"
-        result[-1] = collatz.get(remaining.toString())
+        result.addAll(collatz[remaining.toString()][1..-1])
         break;
       } else {
+        //println "calcing ${remaining}"
         if (remaining % 2 == 0) {
           remaining /= 2
         } else {
           remaining = remaining * 3 + 1
         }
+
+        //println "recursing on ${remaining}"
+        if (!collatz.containsKey(remaining.toString())) {
+          collatz[remaining.toString()] = generateCollatz(remaining)
+        }
+
         result << remaining
       }
     }
 
-    collatz.put(n.toString(), result)
+    //println "found $n = $result"
+    if (!collatz.containsKey(n.toString())) {
+      //println "added mapping $n -> $result"
+      collatz[n.toString()] = result
+    }
+    //println "Map = $collatz"
 
     return result
   }
+}
+package com.tomtresansky.projecteuler.util
 
-  def List getCollatz(n) {
-    if (!collatz.containsKey(n)) {
-      generateCollatz(n)
+class CollatzGenerator {
+  Map collatz = [:].withDefault { key -> generateCollatz(key) }
+
+  private List generateCollatz(n) {
+    assert n > 0
+
+    BigInteger remaining = n
+    List result = [remaining]
+    while (remaining != 1) {
+      //println "testing ${remaining}"
+      if (collatz.containsKey(remaining.toString())) {
+        //println "found ${remaining}"
+        result.addAll(collatz[remaining.toString()][1..-1])
+        break;
+      } else {
+        //println "calcing ${remaining}"
+        if (remaining % 2 == 0) {
+          remaining /= 2
+        } else {
+          remaining = remaining * 3 + 1
+        }
+
+        //println "recursing on ${remaining}"
+        if (!collatz.containsKey(remaining.toString())) {
+          collatz[remaining.toString()] = generateCollatz(remaining)
+        }
+
+        result << remaining
+      }
     }
 
-    return collatz[n]
+    //println "found $n = $result"
+    if (!collatz.containsKey(n.toString())) {
+      //println "added mapping $n -> $result"
+      collatz[n.toString()] = result
+    }
+    //println "Map = $collatz"
+
+    return result
   }
 }
